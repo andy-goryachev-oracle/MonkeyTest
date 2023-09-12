99a0491149fc002f59bc7b54666c1631d10ca30f
Andy Goryachev <andy.goryachev@oracle.com>
2023-09-12 15:18:45 -0700

    css font size

--- src/com/oracle/tools/fx/monkey/tools/CssPlaygroundPane.java
+++ src/com/oracle/tools/fx/monkey/tools/CssPlaygroundPane.java
@@ -25,9 +25,13 @@
 package com.oracle.tools.fx.monkey.tools;
 
 import java.nio.charset.Charset;
+import java.util.Arrays;
 import java.util.Base64;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
@@ -41,49 +45,120 @@ import javafx.stage.Window;
 public class CssPlaygroundPane extends BorderPane {
     private final ColorPicker colorPicker;
     private static String oldStylesheet;
+    private int fontSize = 12;
+    private static final int[] SIZES = {
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        16,
+        18,
+        20,
+        22
+    };
+    private final Label fontSizeLabel;
 
     public CssPlaygroundPane() {
         colorPicker = new ColorPicker();
-        
+
+        Button fsLarger = new Button("+");
+        fsLarger.setOnAction((ev) -> fontSize(true));
+
+        Button fsSmaller = new Button("-");
+        fsSmaller.setOnAction((ev) -> fontSize(false));
+
+        fontSizeLabel = new Label("12");
+        fontSizeLabel.setAlignment(Pos.CENTER);
+
+        BorderPane fs = new BorderPane(fontSizeLabel);
+        fs.setLeft(fsSmaller);
+        fs.setRight(fsLarger);
+
         GridPane p = new GridPane();
+        p.setPadding(new Insets(10));
+        p.setHgap(5);
+        p.setVgap(5);
         int r = 0;
         p.add(new Label("Background Color:"), 0, r);
         p.add(colorPicker, 1, r);
+        r++;
+        p.add(new Label("Font Size:"), 0, r);
+        p.add(fs, 1, r);
         setCenter(p);
-        
+
         colorPicker.setOnAction((ev) -> {
             update();
         });
     }
-    
-    protected void update() {
+
+    private void fontSize(boolean larger) {
+        fontSize = nextFontSize(larger);
+        fontSizeLabel.setText(String.valueOf(fontSize));
+        update();
+    }
+
+    private int nextFontSize(boolean larger) {
+        int ix = indexOf(fontSize);
+        if (ix < 0) {
+            return 12;
+        } else {
+            ix += (larger ? 1 : -1);
+            if (ix < 0) {
+                ix = 0;
+            } else if (ix >= SIZES.length) {
+                ix = (SIZES.length - 1);
+            }
+            return SIZES[ix];
+        }
+    }
+
+    private int indexOf(int val) {
+        for (int i = 0; i < SIZES.length; i++) {
+            if (SIZES[i] == val) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void update() {
         Color c = colorPicker.getValue();
         if (c == null) {
             c = Color.WHITE;
         }
-        
+
         String css = generate(c);
         //System.out.println(css); // FIX
 
         applyStyleSheet(css);
     }
-    
-    protected String generate(Color bg) {
+
+    private String generate(Color bg) {
         StringBuilder sb = new StringBuilder();
         sb.append(".root {\n");
+
         sb.append(" -fx-base: " + toCssColor(bg) + ";\n");
+
+        sb.append("-fx-font-size: ");
+        sb.append(fontSize * 100.0 / 12.0);
+        sb.append("%;\n");
+
         sb.append("}\n");
         return sb.toString();
     }
 
-    protected static String toCssColor(Color c) {
+    private static String toCssColor(Color c) {
         int r = toInt8(c.getRed());
         int g = toInt8(c.getGreen());
         int b = toInt8(c.getBlue());
         return String.format("#%02X%02X%02X", r, g, b);
     }
 
-    protected static int toInt8(double x) {
+    private static int toInt8(double x) {
         int v = (int)Math.round(x * 255);
         if (v < 0) {
             return 0;
@@ -93,7 +168,7 @@ public class CssPlaygroundPane extends BorderPane {
         return v;
     }
 
-    protected static String encode(String s) {
+    private static String encode(String s) {
         if (s == null) {
             return null;
         }
@@ -102,10 +177,10 @@ public class CssPlaygroundPane extends BorderPane {
         return "data:text/css;base64," + Base64.getEncoder().encodeToString(b);
     }
 
-    public static void applyStyleSheet(String styleSheet) {
+    private static void applyStyleSheet(String styleSheet) {
         String ss = encode(styleSheet);
         if (ss != null) {
-            for (Window w: Window.getWindows()) {
+            for (Window w : Window.getWindows()) {
                 Scene scene = w.getScene();
                 if (scene != null) {
                     ObservableList<String> sheets = scene.getStylesheets();
