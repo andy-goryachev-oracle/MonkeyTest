3cece63d3d753a22c975458cc293506909c3218f
Andy Goryachev <andy.goryachev@oracle.com>
2024-01-24 12:00:43 -0800

    scroll pane page

--- src/com/oracle/tools/fx/monkey/Pages.java
+++ src/com/oracle/tools/fx/monkey/Pages.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -56,7 +56,7 @@ public class Pages {
             new DemoPage("PieChart", PieChartPage::new),
             // TODO ProgressIndicator
             new DemoPage("ScrollBar", ScrollBarPage::new),
-            // TODO ScrollPane
+            new DemoPage("ScrollPane", ScrollPanePage::new),
             // TODO Separator
             // TODO Slider
             new DemoPage("Spinner", SpinnerPage::new),
