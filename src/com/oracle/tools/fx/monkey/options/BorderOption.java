342888a50fac57ebacdef5d64530a174069f0359
Andy Goryachev <andy.goryachev@oracle.com>
2024-10-17 11:52:26 -0700

    border option

--- src/com/oracle/tools/fx/monkey/options/BorderOption.java
+++ src/com/oracle/tools/fx/monkey/options/BorderOption.java
@@ -48,6 +48,7 @@ public class BorderOption extends ObjectOption<Border> {
         addChoice("Empty (100)", createBorder(Color.TRANSPARENT, 100, null));
         addChoice("Red (1)", createBorder(Color.RED, 1, null));
         addChoice("Green (20)", createBorder(Color.GREEN, 20, null));
+        addChoice("(T:11, B:22, L:33, R:44)", createBorder(Color.rgb(127, 127, 127, 0.5), 11, 44, 22, 33));
         addChoice("Rounded", createBorder(Color.ORANGE, 1, 5.0));
 
         selectInitialValue();
@@ -57,10 +58,12 @@ public class BorderOption extends ObjectOption<Border> {
         BorderStrokeStyle style = BorderStrokeStyle.SOLID;
         CornerRadii radii = radius == null ? null : new CornerRadii(radius);
         BorderWidths widths = new BorderWidths(width);
+        return new Border(new BorderStroke(color, style, radii, widths));
+    }
 
-        BorderStroke[] strokes = {
-            new BorderStroke(color, style, radii, widths)
-        };
-        return new Border(strokes);
+    private static Border createBorder(Color color, double top, double right, double bottom, double left) {
+        BorderStrokeStyle style = BorderStrokeStyle.SOLID;
+        BorderWidths widths = new BorderWidths(top, right, bottom, left);
+        return new Border(new BorderStroke(color, style, null, widths));
     }
 }
