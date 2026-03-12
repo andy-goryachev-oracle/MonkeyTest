8fabc2685125935d2c9ce4bdd24f60a546c1311e
Andy Goryachev <andy.goryachev@oracle.com>
2026-03-12 09:54:43 -0700

    unused imports

--- src/com/oracle/tools/fx/monkey/tools/StageTesterWindow.java
+++ src/com/oracle/tools/fx/monkey/tools/StageTesterWindow.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2025, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,7 +25,9 @@
 
 package com.oracle.tools.fx.monkey.tools;
 
-import javafx.application.Platform;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 import javafx.collections.FXCollections;
 import javafx.css.PseudoClass;
 import javafx.geometry.Insets;
@@ -45,19 +47,15 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.HeaderBar;
 import javafx.scene.layout.HeaderButtonType;
 import javafx.scene.layout.HeaderDragType;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import javafx.stage.Window;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
 
 public final class StageTesterWindow extends Stage {
 
